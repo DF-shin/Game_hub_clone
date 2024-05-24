6da37a5f9a0292f7dbc80744b828a0effe76c737
Fox <declanschindler@gmail.com>
2024-05-24 15:41:21 -0400

    Cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
-import { Game, Platform } from "./hooks/useGames";
+import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
