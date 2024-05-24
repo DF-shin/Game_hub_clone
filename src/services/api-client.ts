d15e81e68fad991674274d10067288a8006827a2
Fox <declanschindler@gmail.com>
2024-05-24 15:03:35 -0400

    Changed API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '6b0293ea6cf84d88abadca86d04095e9'
+        key: import.meta.env.VITE_API_KEY
     }
 
 })
